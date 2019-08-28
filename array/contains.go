a48c7e8a52b15e7137bad1385ec5761459ce98e2
ywardhana <muhammad.yasher@gmail.com>
2019-08-28 23:18:42 +0700

    array contains

---
+++ array/contains.go
@@ -0,0 +1,10 @@
+package array
+
+func Contains(slice []interface{}, elem interface{}) bool {
+	for _, item := range slice {
+		if item == elem {
+			return true
+		}
+	}
+	return false
+}
---
+++ array/contains_test.go
@@ -0,0 +1,37 @@
+package array_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ywardhana/golib/array"
+)
+
+func TestContains(t *testing.T) {
+	tests := []struct {
+		array    []interface{}
+		item     interface{}
+		expected bool
+	}{
+		{
+			array:    []interface{}{1, 2, 3, 4, 5},
+			item:     5,
+			expected: true,
+		},
+		{
+			array:    []interface{}{1, 2, 3, 4},
+			item:     5,
+			expected: false,
+		},
+		{
+			array:    []interface{}{1, "haha", 3, 4.5},
+			item:     5,
+			expected: false,
+		},
+	}
+
+	for _, tt := range tests {
+		assert.Equal(t, tt.expected, array.Contains(tt.array, tt.item))
+	}
+}
